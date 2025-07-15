b16d36ee897677f26c0a325b39f6dc0dd78d3fa2
praveen-9014 <151435506+praveen-9014@users.noreply.github.com>
2025-07-15 06:31:02 +0530

    Day-9 calculator task is completed

---
+++ CalculatorUsingJunitTesting/src/main/java/com/example/springbootfirst/Calculator.java
@@ -0,0 +1,24 @@
+package com.example.springbootfirst;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("cannot divided by zero");
+        }
+        return a / b;
+    }
+}
+
